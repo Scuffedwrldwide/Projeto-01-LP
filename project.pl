c41fec37409e0dbd03b00ee3d133558b5feafcc5
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2022-12-19 16:47:11 +0000

    TEST

--- project.pl
+++ project.pl
@@ -1,6 +1,5 @@
 % 106537 Francisco Fernandes
 :- set_prolog_flag(answer_write_options,[max_depth(0)]). % para listas completas
-:- [’dados.pl’], [’keywords.pl’]. % ficheiros a importar.
-/*
+:- ['dados.pl'], ['keywords.pl']. % ficheiros a importar.
 
-*/
+%organizaEventos([ID|R], P, E) :- horario(ID,_,_,_,_,P)organizaEventos(R,P,[ID|]).
