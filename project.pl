7bd39745e56c076eda56a285f12888730d104d25
Scuffedwrldwide <kiko2004kiko@gmail.com>
2022-12-19 18:25:58 +0000

    eventosSemSala defined

--- project.pl
+++ project.pl
@@ -2,4 +2,6 @@
 :- set_prolog_flag(answer_write_options,[max_depth(0)]). % para listas completas
 :- ['dados.pl'], ['keywords.pl']. % ficheiros a importar.
 
-%organizaEventos([ID|R], P, E) :- horario(ID,_,_,_,_,P)organizaEventos(R,P,[ID|]).
+% Qualidade dos Dados
+
+eventosSemSalas(EventosSemSala) :- setof(ID, evento(ID,_,_,_,_), EventosSemSala).
