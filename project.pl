42f873c22ba8d7bf7019651a0d2edafa0221a3c2
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2022-12-19 20:01:09 +0000

    organizaEventos

--- project.pl
+++ project.pl
@@ -4,5 +4,13 @@
 
 % Qualidade dos Dados
 eventosSemSalas(EventosSemSala) :-  findall(ID, evento(ID, _, _, _, semSala), EventosSemSala).
+%usa maplist no prox.
+
+% Pesquisas Simples
+organizaEventos([], Periodo, EventosNoPeriodo) :- true.              % Caso Terminal
+organizaEventos([ID|R], Periodo, EventosNoPeriodo) :- horario(ID, _, _, _, _, Periodo),     
+                                                     organizaEventos(R, Periodo, [ID|_]); %NOT WORKING YET, RETURNS TRUE
+                                                     organizaEventos(R, Periodo, [_]).
+
 
                                     
