2ade4740c40609496f3c41c56f02628e3d854117
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2022-12-20 15:34:22 +0000

    fixed organizaEventos + multiple period fix

--- project.pl
+++ project.pl
@@ -13,11 +13,19 @@ eventosSemSalasDiaSemana(DiaSemana, EventosSemSala) :- (eventosSemSalas([ID|_]),
 
 
 % Pesquisas Simples
+organizaEventos(Eventos, Periodo, EventosNoPeriodo) :-
+    organizaEventos(Eventos, Periodo, [], EventosNoPeriodo).        % Inclui uma variável acumuladora para os periodos encontrados
+
+/* Os eventos com mais que um periodo resultam da concatenação de, eg., p1_p2. 
+   o predicado ehPeriodo/2 permite verificar a pertença a um destes periodos  
+   sendo Periodo o periodo desejado e P a variavel constante no horario/6     */
+ehPeriodo(Periodo, P) :-
+    atom_concat(Periodo, '_', Outro), atom_concat(Outro, '_', Periodo),
 
 organizaEventos([], _, EventosNoPeriodo, EventosNoPeriodo).
 organizaEventos([ID|R], Periodo, Acc, EventosNoPeriodo) :-
     horario(ID, _, _, _, _, P),
-    (P = Periodo ->
+    (ehPeriodo(Periodo, P) ->
         organizaEventos(R, Periodo, [ID|Acc], EventosNoPeriodo)
     ;
         organizaEventos(R, Periodo, Acc, EventosNoPeriodo)
