cb7c24c2326326e683e77f5b3e445393a819fab4
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2022-12-20 20:08:37 +0000

    OrganizaEventos small adaptation

--- project.pl
+++ project.pl
@@ -5,29 +5,34 @@
 % Qualidade dos Dados
 eventosSemSalas(EventosSemSala) :-  findall(ID, evento(ID, _, _, _, semSala), EventosSemSala).
 
-eventosSemSalasDiaSemana(DiaSemana, EventosSemSala) :-
-    eventosSemSalasDiaSemana(DiaSemana, [], EventosSemSala).     % Inclui uma variável acumuladora para os eventos encontrados
-eventosSemSalasDiaSemana(_, EventosSemSala, EventosSemSala).     % Conclui a execução quando a variável acumuladora é unificada com os eventos sem sala
-eventosSemSalasDiaSemana(DiaSemana, Acc, EventosSemSala) :-
-    eventosSemSalas([ID|R]),                                    % NOT EXCLUDING ID ON NEXT LOOP, FIX LATER
+
+eventosSemSalasDiaSemana(DiaSemana, Eventos) :-
+    eventosSemSalasDiaSemana(DiaSemana, [], Eventos).           % Inclui uma variável acumuladora para os eventos encontrados
+
+eventosSemSalasDiaSemana(_, Eventos, Eventos).    
+eventosSemSalasDiaSemana(DiaSemana, Acc, Eventos) :-
+    (
+    eventosSemSalas(Eventos),                                   % NOT EXCLUDING ID ON NEXT LOOP, FIX LATER
+    ([ID|R] = Eventos),
     horario(ID, DiaSemana, _, _, _, _),
-    eventosSemSalasDiaSemana(DiaSemana, [ID|Acc], EventosSemSala).
+    eventosSemSalasDiaSemana(DiaSemana, [ID|Acc], R));
+    eventosSemSalasDiaSemana(DiaSemana, Acc, R).
 
 
 % Pesquisas Simples
 organizaEventos(Eventos, Periodo, EventosNoPeriodo) :-
-    organizaEventos(Eventos, Periodo, [], EventosNoPeriodo).    % Inclui uma variável acumuladora para os periodos encontrados
-
+    organizaEventos(Eventos, Periodo, [], ToReverse), reverse(ToReverse, EventosNoPeriodo).    % Inclui uma variável acumuladora para os periodos encontrados
+                                                        % MIGHT BE ABLE TO REMOVE THIS
 /* Os eventos com mais que um periodo resultam da concatenação de, eg., p1_p2. 
    o predicado ehPeriodo/2 permite verificar a pertença a um destes periodos  
    sendo Periodo o periodo desejado e P a variavel constante no horario/6     */
 ehPeriodo(Periodo, P) :-
-    atom_concat(Periodo, '_', Outro), atom_concat(Outro, '_', Periodo),
+    sub_atom(P, Before, 2, After, Periodo).
 
 organizaEventos([], _, EventosNoPeriodo, EventosNoPeriodo).
 organizaEventos([ID|R], Periodo, Acc, EventosNoPeriodo) :-
     horario(ID, _, _, _, _, P),
-    (ehPeriodo(Periodo, P) ->
+    ((P = Periodo; ehPeriodo(Periodo, P)) -> 
         organizaEventos(R, Periodo, [ID|Acc], EventosNoPeriodo)
     ;
         organizaEventos(R, Periodo, Acc, EventosNoPeriodo)
