2f6203de54bdae0e4ea40dcd9d530f2b4984dcb1
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2022-12-23 19:47:15 +0000

    eventosSemSalas rework

--- project.pl
+++ project.pl
@@ -6,7 +6,23 @@
 eventosSemSalas(EventosSemSala) :-  findall(ID, evento(ID, _, _, _, semSala), EventosSemSala).
 
 
-eventosSemSalasDiaSemana(DiaSemana, Eventos) :-
+eventosSemSalasDiaSemana(DiaSemana, Eventos) :- findall(ID, horario(ID, DiaSemana, _, _, _, _), NoDia),
+                                                eventosSemSalas(EventosSemSala),
+                                                intersection(NoDia, EventosSemSala, Eventos).
+
+
+eventosSemSalasPeriodo([], []).
+eventosSemSalasPeriodo([Periodo|R], [Evento|Outros]) :-
+                                              
+                                              findall(ID, (horario(ID, _, _, _, _, P), 
+                                                           ehPeriodo(Periodo, P)), 
+                                                           NoPeriodo),
+                                              eventosSemSalas(EventosSemSala),
+                                              intersection(NoPeriodo, EventosSemSala, Evento),
+                                              %append(Corrente, Evento),
+                                              eventosSemSalasPeriodo(R, Outros).
+
+/*eventosSemSalasDiaSemana(DiaSemana, Eventos) :-
     eventosSemSalasDiaSemana(DiaSemana, [], Eventos).           % Inclui uma variável acumuladora para os eventos encontrados
 
 eventosSemSalasDiaSemana(_, Eventos, []) :- Eventos \= [] .     % Caso terminal, impede a unificação de Eventos com []
@@ -19,6 +35,20 @@ eventosSemSalasDiaSemana(DiaSemana, Acc, [_|R]) :-              % Caso o ID não
     eventosSemSalasDiaSemana(DiaSemana, Acc, R).                % Avança-se recursiamente para os restantes eventos
 
 
+eventosSemSalasPeriodo(Periodo, Eventos) :-
+    eventosSemSalasPeriodo(Periodo, [], Eventos).               % Inclui uma variável acumuladora para os eventos encontrados
+
+eventosSemSalasPeriodo(_, Eventos, []) :- Eventos \= [] .       % Caso terminal, impede a unificação de Eventos com []
+eventosSemSalasPeriodo([Periodo | Next] , Acc, [ID|R]) :-
+    eventosSemSalas(EventosSemSala),
+    member(ID, EventosSemSala),                                 % Verifica a correspondencia de um ID a um evento sem sala
+    horario(ID, _, _, _, _, P),
+    ehPeriodo(Periodo, P),                                      % Um evento pode ocorrer em multiplos períodos
+    eventosSemSalasPeriodo(Next, [ID|Acc], R).                  % Adiciona o ID encontrado ao acumulador
+eventosSemSalasPeriodo([Periodo | Next], Acc, [_|R]) :-         % Caso o ID não corresponda, este não é adicionado
+    eventosSemSalasPeriodo(Next, Acc, R).                       % Avança-se recursiamente para os restantes eventos
+*/
+
 
 % Pesquisas Simples
 organizaEventos(Eventos, Periodo, EventosNoPeriodo) :-
@@ -29,7 +59,7 @@ organizaEventos(Eventos, Periodo, EventosNoPeriodo) :-
    sendo Periodo o periodo desejado e P a variavel constante no horario/6     */
 ehPeriodo(Periodo, P) :-
     sub_atom(Periodo, 1, 1, After, Num),    % Verifica que existe um Numero no periodo dado
-    sub_atom(P, Before, 1, After, Num).     % e que esse numero está presente no horario
+    sub_atom(P, _, 1, After, Num).     % e que esse numero está presente no horario
 
 organizaEventos([], _, EventosNoPeriodo, EventosNoPeriodo).
 organizaEventos([ID|R], Periodo, Acc, EventosNoPeriodo) :-
@@ -57,9 +87,10 @@ eventosMenoresQueBool(ID, Duracao) :- horario(ID, _, _, _, Time, _), Time =< Dur
 /*TODO
 
 - Implement own sorting system
-- Slim down ehPeriodo
-- Fix whatever the fuck is up with Eventos
+- Slim down ehPeriodo √
+- Fix whatever the fuck is up with Eventos √
 - less reverse() use
 - figure out singletons
+- less findall()
 
 */
