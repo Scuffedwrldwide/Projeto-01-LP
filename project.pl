d078a52af9aa211476eff4b2fd9a21d1761ec831
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2022-12-24 20:11:56 +0000

    procura + organiza + aux

--- project.pl
+++ project.pl
@@ -5,12 +5,10 @@
 % Qualidade dos Dados
 eventosSemSalas(EventosSemSala) :-  findall(ID, evento(ID, _, _, _, semSala), EventosSemSala).
 
-
 eventosSemSalasDiaSemana(DiaSemana, Eventos) :- 
     findall(ID, horario(ID, DiaSemana, _, _, _, _), NoDia),
     eventosSemSalas(EventosSemSala),
     intersection(NoDia, EventosSemSala, Eventos).
-
 
 eventosSemSalasPeriodo([], []).
 eventosSemSalasPeriodo([Periodo|R], [Evento|Outros]) :-
@@ -41,20 +39,56 @@ eventosMenoresQueBool(ID, Duracao) :- horario(ID, _, _, _, Time, _), Time =< Dur
 eventosMenoresQue(Duracao, ListaEventosMenoresQue) :-
     findall(ID, eventosMenoresQueBool(ID, Duracao), ListaEventosMenoresQue).
 
+procuraDisciplinas(Curso, ListaDisciplinasCurso) :-
+    idsCurso(Curso, ListaTurnosCurso),
+    discFinder(ListaTurnosCurso, ListaDisciplinasCurso).
 
+organizaDisciplinas(ListaDisciplinas, Curso, [Sem1, Sem2]) :-
+    idsCurso(Curso, ListaIDsCurso),
+    organizaEventos(ListaIDsCurso, p1, IDsP1), organizaEventos(ListaIDsCurso, p2, IDsP2),
+        union(IDsP1, IDsP2, IDsSem1),
+    organizaEventos(ListaIDsCurso, p3, IDsP3), organizaEventos(ListaIDsCurso, p4, IDsP4),
+        union(IDsP3, IDsP4, IDsSem2),
+    discFinder(IDsSem1, TotalDisc1), discFinder(IDsSem2, TotalDisc2),
+        intersection(ListaDisciplinas, TotalDisc1, Sem1), 
+        intersection(ListaDisciplinas, TotalDisc2, Sem2),
+    Sem1 \= [], Sem2 \= [].
+    
 
 % Auxiliares
+
+discFinder(ListaTurnos, ListaDisciplinas) :-
+    findall(Disciplina, (member(ID, ListaTurnos), evento(ID, Disciplina, _, _, _)), Lista),
+    sort(Lista, ListaDisciplinas).
+idsCurso(Curso, ListaIDs) :-
+    findall(ID, turno(ID, Curso, _, _), ListaIDs).
+
+/*discFinder([], _).
+discFinder([ID|R1], [Disciplina|R2]) :-
+    evento(ID, Disciplina, _, _, _),
+    \+ member(Disciplina, R2),
+    discFinder(R1, R2).
+discFinder([ID|R1], R2) :-
+    evento(ID, _, _, _, _),
+    discFinder(R1, R2).*/
 
 
 bubbleSort(ToSort, Sorted) :-
     switcharoo(ToSort, Sort1), !,       % Apos a troca de elementos, a chamada recursiva  %
     bubbleSort(Sort1, Sorted).          % e efetuada ate nao serem possiveis mais trocas. %
-
 bubbleSort(Sorted, Sorted).             % Caso terminal, no qual a lista aordenar e a ordenada sao iguais.
 
 switcharoo([X, Y|R], [Y,X|R]) :- X > Y. % Caso base, no qual a troca de elementos e necessaria.
 switcharoo([Z|R1], [Z|R2]) :-           % Caso recursivo, que 'investiga' a lista em profundidade.
     switcharoo(R1, R2).                    
+
+/*duplicateRemover([], []).
+duplicateRemover([X|R], Result) :-
+    member(X, R),
+    duplicateRemover(R, Result).
+duplicateRemover([X|R], [X|Result]) :-
+    \+ member(X, R),
+    duplicateRemover(R, Result).*/
 
 
 
